7b06e811df268e2d50eb6aca4bbb69f3c20489e5
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-01-12 22:01:18 -0800

    tasks complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// });
+let allTasks = [
+  {
+    "id":0,
+    "description":"Surfing",
+  }, 
+  {
+    "id":1,
+    "description":"Snowboarding", 
+  }
+]
+
+//function for DOM elements 
+function addToTaskList(task){
+  let ul = document.querySelector('#tasks')
+  let li = document.createElement('li')
+  li.textContent = task.description
+  ul.appendChild(li)
+}
+
+
+//adding handles 
+function handleSubmit(e){
+  e.preventDefault() 
+  let task = {
+    "description": e.target.new_task.value
+  }
+  addToTaskList(task)
+//adding event listeners
+}
+
+
+function addListeners(){
+  // listener for sumbit form
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit',handleSubmit)
+}
+
+addListeners()
